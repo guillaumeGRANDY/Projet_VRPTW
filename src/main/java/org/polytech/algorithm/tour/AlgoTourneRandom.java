03bdc462789a24fc5b2fb6c49f77d4ab3ae79eb2
Mathis Poncet-Crouzet <mathis.poncet-crouzet@sncf.fr>
2024-04-22 17:24:48 +0200

    fix: make random generation more random

--- src/main/java/org/polytech/algorithm/tour/AlgoTourneRandom.java
+++ src/main/java/org/polytech/algorithm/tour/AlgoTourneRandom.java
@@ -6,6 +6,8 @@ import org.polytech.model.*;
 import java.util.*;
 
 public class AlgoTourneRandom extends AlgoTourne {
+    private Random random = new Random();
+
     public AlgoTourneRandom(ConstraintTruck constraintTruck, Depot depot, List<Client> clients) {
         super(constraintTruck, depot, clients);
         this.clientsToServe.sort(new HeureDebutClientComparator());
@@ -28,15 +30,16 @@ public class AlgoTourneRandom extends AlgoTourne {
         double time = 0;
         int indiceClient = 0;
         int timeByDistance=0;
+        int visitedClients = 0;
 
         Location currentPostion = depot;
-        while (this.clientsToServe.isEmpty() == false && indiceClient < this.clientsToServe.size()) {
+        while (this.clientsToServe.isEmpty() == false && route.getTruck().getPlaceRemaning() > 0 && visitedClients < this.clientsToServe.size()){
+            indiceClient = random.nextInt(this.clientsToServe.size());
             boolean capacityIsUsed = route.tryAddNewLivraison(new Livraison(this.clientsToServe.get(indiceClient), this.clientsToServe.get(indiceClient).getReadyTime() + timeByDistance * currentPostion.distanceWith(this.clientsToServe.get(indiceClient))));
             if (capacityIsUsed) {
                 currentPostion = this.clientsToServe.remove(indiceClient);
-            } else {
-                indiceClient++;
             }
+            visitedClients++;
         }
         return route;
     }
